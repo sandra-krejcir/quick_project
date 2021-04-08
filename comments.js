b6d017fa63e72902603252586ff0c23152c2fbc8
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-04-08 19:03:38 +0200

    adding java to comments form

---
+++ comments.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", userSubmitted);
+
+function userCommented(evt) {
+  evt.preventDefault();
+  console.log(form.elements.username.value);
+  console.log(form.elements.content.value);
+
+  const payload = {
+    username: form.elements.username.value,
+    content: form.elements.content.value,
+  };
+
+  document.querySelector(".form").disabled = true;
+
+  fetch("https://kea21s-6eb0.restdb.io/rest/comments", {
+    method: "POST",
+    headers: {
+      "x-apikey": "606d606af55350043100752e",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => {
+      console.log(response);
+      document.querySelector(".form").disabled = false;
+      form.elements.username.value = "";
+      form.elements.content.value = "";
+      document.querySelector("p.hidden").classList.remove("hidden");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
